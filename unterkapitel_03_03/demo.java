bc4fe8b0288cd9d07027dd478bdfa546822c12a8
bacaiib <roman-frommhold@gmx.net>
2024-03-16 17:51:05 +0100

    test

---
+++ unterkapitel_03_03/demo.java
@@ -0,0 +1,19 @@
+package unterkapitel_03_03;
+
+public class demo {
+
+    public static void main(String[] args) {
+ 
+        int c = berechne(2,7);
+        System.out.println(c);
+    }
+
+    public static int berechne(int a, int b){
+        a+=b;
+        return a;
+    }
+
+  
+
+
+}
